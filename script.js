596ef0f839547d9131716dfb82700a8ee09caf21
Ani Kurdadze <anikurdadze0@gmail.com>
2024-08-06 16:22:12 +0400

    s

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let title = document.querySelector("h1");
 title.textContent = "ვენეცია";
 title.className = "title";
+
 let pText = document.querySelector("p");
 pText.textContent = "ვენეცია ვენეტოს რეგიონის დედაქალია. სახელი მომდინარეობს აქ მაცხოვრებელი ანტიკური პერიოდის ხალხისგან, რომლებსაც ვენეტები ეწოდებოდათ.";
 pText.style.fontSize = "20px";
 pText.style.color = "#ffff00";
+
 let circle = document.createElement("section");
 circle.className = "circle";
 let box = document.querySelector("div");
 box.appendChild(circle);
+
 circle.addEventListener("mouseover", () => {
     circle.style.backgroundColor = "red"
 });
